efe203b52a5e30c54a23706371c64f0baeac145d
Masum Hasan <youngladesh@gmail.com>
2023-09-27 11:03:39 -0400

    update

--- Workshop2/ChatGPT_API_Flask_App/chat-api-format.py
+++ Workshop2/ChatGPT_API_Flask_App/chat-api-format.py
@@ -1,10 +1,16 @@
 import os
 import openai
-openai.api_key = 'YOUR_API_KEY'
-openai.Completion.create(
-  model="gpt-3.5-turbo-instruct",
-  prompt="Say this is a test",
-  max_tokens=7,
-  temperature=0
+openai.api_key = "OPENAI_API_KEY"
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hello!"}
+  ],
+  max_tokens=100,
+  temparature=0
 )
+
+print(completion.choices[0].message)
 
--- Workshop2/README.md
+++ Workshop2/README.md
@@ -7,3 +7,8 @@ ChatGPT API docs:
 
 Sign up: https://platform.openai.com/signup
 Chat API: https://platform.openai.com/docs/api-reference/chat
+Pricing: https://openai.com/pricing#language-models
+
+
+Colab: https://colab.research.google.com/drive/18Im68smW9_JHHHB49vClfHIe7W_UbCnx?usp=sharing
+Full project: https://github.com/Masum06/ChatGPT-workshop/tree/main/Workshop2
